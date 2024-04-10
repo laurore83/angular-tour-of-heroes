9902ded915ade8bab2290c7b1374e8c4c2c25bd6
Laurore83 <laurore83@yahoo.fr>
2024-04-10 14:54:45 +0200

    blocks done

---
+++ src/app/Block/block.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'app-block',
+  templateUrl: './block.component.html',
+  styleUrl: './block.component.css',
+})
+export class BlockComponent {
+  block: string = 'A B C';
+}
